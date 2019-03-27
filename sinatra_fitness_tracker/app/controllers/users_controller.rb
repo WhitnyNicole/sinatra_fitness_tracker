1b1a077e693dfa9ae06ceb8415bc4e2fcdc486ba
Whitny Edwards <wne4e2@gmail.com>
2019-03-26 22:36:01 -0400

    update controller files

--- sinatra_fitness_tracker/app/controllers/users_controller.rb
+++ sinatra_fitness_tracker/app/controllers/users_controller.rb
@@ -1,37 +1,92 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get '/users/signup' do
+    erb :'/users/signup.html'
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  post '/users/signup' do
+    user = User.new(params)
+    if user.save
+      session[:user_id] = user.id
+      redirect "/workouts"
+      # redirect "/users/#{user.id}"
+    else
+      redirect '/users/signup'
+    end
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
+  get '/users/login' do
+     "you are logged in"
+     if is_logged_in?
+       redirect '/workouts/show'
+     else
+       "Please log into your account"
+       erb :'/users/login.html'
+     end
+   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+   post '/users/login' do
+     user = User.find_by(:username => params[:username])
+     if user && user.authenticate(params[:password])
+         session[:user_id] = user.id
+     end
+         redirect to '/workouts/show'
+   end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+    # POST: /users
+    post "/users" do
+      user = User.new(params)
+      user.save
+      redirect "/users/#{user.id}"
+    end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
-end
+    # GET: /users/5
+    get "/users/:id" do
+      @user = User.find(params[:id])
+      erb :"/users/show.html"
+    end
+
+    # GET: /users/5/edit
+    get "/users/:id/edit" do
+      erb :"/users/edit.html"
+    end
+
+
+
+
+#   # GET: /users
+#   get "/users" do
+#     erb :"/users/index.html"
+#   end
+#
+#   # GET: /users/new
+#   get "/users/new" do
+#     erb :"/users/new.html"
+#   end
+#
+#   # POST: /users
+#   post "/users" do
+#     redirect "/users"
+#   end
+#
+#   # GET: /users/5
+#   get "/users/:id" do
+#     erb :"/users/show.html"
+#   end
+#
+#   # GET: /users/5/edit
+#   get "/users/:id/edit" do
+#     erb :"/users/edit.html"
+#   end
+#
+#   # PATCH: /users/5
+#   patch "/users/:id" do
+#     redirect "/users/:id"
+#   end
+#
+#   # DELETE: /users/5/delete
+#   delete "/users/:id/delete" do
+#     redirect "/users"
+#   end
+# end
--- sinatra_fitness_tracker/app/models/user.rb
+++ sinatra_fitness_tracker/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :workouts
+  has_secure_password
+
+  validates :name, :email, presence: true
 end
