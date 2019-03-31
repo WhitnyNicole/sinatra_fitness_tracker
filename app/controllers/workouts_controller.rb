167e1baddb7b47b51748f6622b278a245138aeaf
Whitny Edwards <wne4e2@gmail.com>
2019-03-30 22:11:57 -0400

    create delete action and add delete link to workouts show page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -4,7 +4,6 @@ class WorkoutsController < ApplicationController
     @workouts = Workout.all
     erb :"/workouts/index"
   end
-#
 
   get "/workouts/new" do
     erb :"workouts/new"
@@ -22,7 +21,6 @@ class WorkoutsController < ApplicationController
     end
   end
 
-
   # post "/workouts" do
   #   workout = Workout.new(params)
   #   if workout.save
@@ -36,7 +34,6 @@ class WorkoutsController < ApplicationController
   #   end
   # end
 
-
   get "/workouts/:id" do
     set_workout
     erb :"workouts/show"
@@ -45,7 +42,7 @@ class WorkoutsController < ApplicationController
   get "/workouts/:id/edit" do
     set_workout
     if logged_in?
-      if @workut.user == current_user
+      if @workout.user == current_user
         erb :"workouts/edit"
       else
         redirect "users/#{current_user.id}"
@@ -55,30 +52,34 @@ class WorkoutsController < ApplicationController
     end
   end
 
-
   patch "/workouts/:id" do
     set_workout
     if logged_in?
-      if @workut.user == current_user
+      if @workout.user == current_user
         @workout.update(category: params[:category])
         redirect "/workouts/#{@workout.id}"
       else
         redirect "users/#{current_user.id}"
       end
     else
+      flash[:message] = "Sorry, you cannot edit that entry."
       redirect "/"
     end
   end
 
-#
-#   # DELETE: /workouts/5/delete
-#   delete "/workouts/:id/delete" do
-#     redirect "/workouts"
-#   end
-
-
-private
-  def set_workout
-    @workout = Workout.find(params[:id])
+  delete "/workouts/:id" do
+    set_workout
+    if current_user == @workout.user
+      @workout.category.destroy
+      flash[:message] = "That entry was successfully deleted."
+      redirect "/workouts"
+    else
+      flash[:message] = "Sorry, you cannot delete that entry."
+      redirect "/workouts"
   end
-end
+
+  private
+    def set_workout
+      @workout = Workout.find(params[:id])
+    end
+  end
