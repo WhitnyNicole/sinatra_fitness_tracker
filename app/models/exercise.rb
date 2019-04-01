0b6ae44e130781610c1576bd6b3481617be0f500
Whitny Edwards <wne4e2@gmail.com>
2019-03-31 20:51:42 -0400

    update exercise index page

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,5 +3,8 @@ class Exercise < ActiveRecord::Base
   def formatted_created_at_time
     self.created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
-  
+
+  def formatted_day
+    self.created_at.strftime("%A, %b %d")
+  end
 end
