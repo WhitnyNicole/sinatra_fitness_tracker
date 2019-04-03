a2a739d5a6f001d8934ba4d7235d8701c2923784
Whitny Edwards <wne4e2@gmail.com>
2019-04-02 23:47:21 -0400

    update controllers spacing and update application controller to redirect a user that is already logged in to their home page

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,48 +6,47 @@ class ExercisesController < ApplicationController
     end
 
 
-  get "/exercises/new" do
-    erb :"/exercises/new"
-  end
-
-  post "/exercises" do
-    redirect_if_not_logged_in
-    if params[:reps] && params[:weight] && params[:day] && params[:intensity] && params[:move] && params[:sets] && params[:workout_id]!= ""
-      @exercise = Exercise.new(params)
-      @exercise.save
-      flash[:message] = "Congrats, you've entered a new exercise!"
-      redirect "/exercises/#{@exercise.id}"
-    else
-      flash[:errors] = "Oops, that exercise could not be created."
-      redirect "/exercises/new"
+    get "/exercises/new" do
+      erb :"/exercises/new"
     end
-  end
+
+    post "/exercises" do
+      redirect_if_not_logged_in
+      if params[:reps] && params[:weight] && params[:day] && params[:intensity] && params[:move] && params[:sets] && params[:workout_id]!= ""
+        @exercise = Exercise.new(params)
+        @exercise.save
+        flash[:message] = "Congrats, you've entered a new exercise!"
+        redirect "/exercises/#{@exercise.id}"
+      else
+        flash[:errors] = "Oops, that exercise could not be created."
+        redirect "/exercises/new"
+      end
+    end
 
 
-  get "/exercises/:id" do
-    set_exercise
-    erb :"exercises/show"
-  end
+    get "/exercises/:id" do
+      set_exercise
+      erb :"exercises/show"
+    end
 
-  get "/exercises/:id/edit" do
-    set_exercise
-    redirect_if_not_logged_in
-    if @exercise.workout.user == current_user
+    get "/exercises/:id/edit" do
+      set_exercise
+      redirect_if_not_logged_in
+      if @exercise.workout.user == current_user
         erb :"exercises/edit"
-    else
-      redirect "users/#{current_user.id}"
+      else
+        redirect "users/#{current_user.id}"
+      end
     end
-  end
 
     patch "/exercises/:id" do
       set_exercise
       redirect_if_not_logged_in
-        if @exercise.workout.user == current_user && params[:reps] && params[:weight] && params[:day] && params[:intensity] && params[:move] && params[:sets] != ""
-          binding.pry
-           @exercise.update(params)
-           flash[:message] = "Your changes were saved!"
-          redirect "/exercises/#{exercise.id}"
-        else
+      if @exercise.workout.user == current_user && params[:reps] && params[:weight] && params[:day] && params[:intensity] && params[:move] && params[:sets] != ""
+        @exercise.update(params)
+        flash[:message] = "Your changes were saved!"
+        redirect "/exercises/#{exercise.id}"
+      else
         flash[:errors] = "Sorry, you cannot edit that entry."
         redirect "users/#{current_user.id}"
       end
@@ -58,10 +57,10 @@ class ExercisesController < ApplicationController
       if @exercise.workout.user == current_user
         @exercise.destroy
         flash[:message] = "That entry was successfully deleted."
-      redirect "/exercises"
+        redirect "/exercises"
       else
-      flash[:errors] = "Sorry, you cannot delete that entry."
-      redirect "/exercises"
+        flash[:errors] = "Sorry, you cannot delete that entry."
+        redirect "/exercises"
       end
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,61 +1,44 @@
 class UsersController < ApplicationController
 
-  #go to individual user's homepage
-  #add the protection
     get '/users/:id' do
       @user = User.find_by(id: params[:id])
       erb :'users/show'
     end
 
-    #log user out
-      get '/logout' do
+    get '/logout' do
         session.clear
         redirect '/'
       end
 
-#create user and sign up
-  get '/signup' do
-    erb :'users/signup'
-  end
+    get '/signup' do
+      erb :'users/signup'
+    end
 
-  post '/users' do
-    @user = User.new(params)
-    if @user.save
-      session[:user_id] = @user.id
-      # redirect "workouts/show"
-      flash[:message] = "You have successfully created an account. Welcome!"
-      redirect "/users/#{@user.id}"
-    else
-      flash[:errors] = "Sorry, please create an account. Must enter name, email and password"
-      redirect 'signup'
+    post '/users' do
+      @user = User.new(params)
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:message] = "You have successfully created an account. Welcome!"
+        redirect "/users/#{@user.id}"
+      else
+        flash[:errors] = "Sorry, please create an account. Must enter name, email and password"
+        redirect 'signup'
+      end
+    end
+
+    get '/login' do
+      erb :'users/login'
+    end
+
+    post '/login' do
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:message] = "Welcome, #{@user.name}!"
+        redirect "users/#{@user.id}"
+      else
+        flash[:errors] = "Your credentials were invalid.  Please sign up or try again."
+        redirect "login"
+      end
     end
   end
-
-  #alternate code to ensure all info filled out
-#   post '/users' do
-#     if params[:name] != "" && params[:email] != "" && params[:password] != ""
-#       @user = User.new(params)
-#       session[:user_id] = @user.id
-#       redirect "users/#{@user.id}"
-#     else
-#       redirect 'users/signup'
-#   end
-# end
-
-#allow returning user to log back in
-  get '/login' do
-    erb :'users/login'
-  end
-
-  post '/login' do
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      flash[:message] = "Welcome, #{@user.name}!"
-      redirect "users/#{@user.id}"
-    else
-      flash[:errors] = "Your credentials were invalid.  Please sign up or try again."
-      redirect "login"
-    end
-  end
-end
