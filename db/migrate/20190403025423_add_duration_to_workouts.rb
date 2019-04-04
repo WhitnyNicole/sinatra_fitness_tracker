0839a570939acbec82a19891981d52fcc93e89d3
Whitny Edwards <wne4e2@gmail.com>
2019-04-03 23:35:03 -0400

    update db data that was entered before additional table columns were added for workouts and exercises

--- db/migrate/20190403025423_add_duration_to_workouts.rb
+++ db/migrate/20190403025423_add_duration_to_workouts.rb
@@ -1,5 +1,5 @@
 class AddDurationToWorkouts < ActiveRecord::Migration
   def change
-    add_column :workouts, :duration, :integer
+    add_column :workouts, :duration, :integer, default: 30
   end
 end
