2b78bd22d0031a9afb06bb8deb6c3dc26be0133e
Whitny Edwards <wne4e2@gmail.com>
2019-04-04 22:13:15 -0400

    add additional input validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,8 +1,8 @@
 class Exercise < ActiveRecord::Base
   belongs_to :workout
-  
+
   def formatted_created_at_time
-    self.created_at.strftime("%A, %d %b %Y %l:%M %p")
+    self.created_at.strftime("%A, %b %d")
   end
 
   def formatted_day
