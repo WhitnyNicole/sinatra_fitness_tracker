5811cd03443ad2b1e04cd5cda8ae6f3584e7bb0b
Whitny Edwards <wne4e2@gmail.com>
2019-04-05 21:59:22 -0400

    remove pattern validation for email address when signing up and logging in, removed unnessary methods from workouts and exercises models

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,11 +1,3 @@
 class Exercise < ActiveRecord::Base
   belongs_to :workout
-
-  def formatted_created_at_time
-    self.created_at.strftime("%A, %b %d")
-  end
-
-  def formatted_day
-    self.created_at.strftime("%A, %b %d")
-  end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,4 @@
 class Workout < ActiveRecord::Base
   belongs_to :user
   has_many :exercises
-  def formatted_created_at_time
-    self.created_at.strftime("%A, %b %d")
-  end
 end
