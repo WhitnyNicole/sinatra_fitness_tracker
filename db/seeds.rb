bd83d7ab2309e2326f4371f92679aa206ead7855
Whitny Edwards <wne4e2@gmail.com>
2019-04-08 22:29:24 -0400

    update db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-whitny = User.create(name: "Whitny", email: "whitny@test.com", password: "password")
+User.create(name: "Whitny", email: "whitny@test.com", password: "password")
 User.create(name: "Britny", email: "britny@test.com", password: "password")
 
 Workout.create(category: "Upper Body", user_id: whitny.id)
